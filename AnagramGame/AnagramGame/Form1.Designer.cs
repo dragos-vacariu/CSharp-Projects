a3429a9aedfa8f7026276e6a3d3a2740de1e09fd
dragos-vacariu <blacky_d2@mail.com>
2017-03-08 22:34:24 +0200

    Anagram Game
    developed in c#

---
+++ AnagramGame/AnagramGame/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace AnagramGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(90, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Quit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label7.ForeColor = System.Drawing.SystemColors.Control;
+            this.label7.Location = new System.Drawing.Point(87, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Type the answer:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(74, 111);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(119, 20);
+            this.textBox7.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Control;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(90, 137);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Check!";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox4.Enabled = false;
+            this.textBox4.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox4.Location = new System.Drawing.Point(12, 22);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(12, 6);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Anagram:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(252, 5);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(20, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(169, 6);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Chances Left:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.ForeColor = System.Drawing.SystemColors.Control;
+            this.label6.Location = new System.Drawing.Point(120, 44);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Info:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Enabled = false;
+            this.textBox6.Location = new System.Drawing.Point(12, 60);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(260, 20);
+            this.textBox6.TabIndex = 13;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label8.ForeColor = System.Drawing.SystemColors.Control;
+            this.label8.Location = new System.Drawing.Point(205, 35);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Level:";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Enabled = false;
+            this.textBox8.Location = new System.Drawing.Point(252, 33);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(20, 20);
+            this.textBox8.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Control;
+            this.button4.Enabled = false;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Location = new System.Drawing.Point(197, 227);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Next ->";
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Enabled = false;
+            this.textBox9.Location = new System.Drawing.Point(167, 188);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(105, 20);
+            this.textBox9.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label9.ForeColor = System.Drawing.SystemColors.Control;
+            this.label9.Location = new System.Drawing.Point(108, 191);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Message:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AnagramGame";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ AnagramGame/AnagramGame/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AnagramGame
+{
+    public partial class Form1 : Form
+    {
+        public string user_input = "";
+        //Use this for levels.
+        List<Anagrams> Game = new List<Anagrams> { };
+        uint level = 1;
+        bool gameover = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Suspending Old Controls:
+            this.button2.Dispose(); //Destroy button2 (Quit);
+            this.button1.Dispose(); //Destroy button1 (Start Game);
+            this.button1.SuspendLayout();
+            this.button2.SuspendLayout();
+
+            //Adding those controls:
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.button4);
+            this.BackgroundImage = global::AnagramGame.Properties.Resources.Green_Light_Keyboard_Macro_Wallpaper_Desktop;
+            //Adding levels:
+            Game.Add(new Anagrams("SRKENADS", "DARKNESS", "It comes once with the night!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("WMTORORO", "TOMORROW", "It comes in future!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("DNGRUNDOUER", "UNDERGROUND", "Its placed below the surface!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("MNETRTPAA", "APARTMENT", "A place to stay!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("TNOCELRACAEI", "ACCELERATION", "Force of a car!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("SERPEDISNO", "DEPRESSION", "Excessive sadness!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("NTLGENCEINIEL", "INTELLIGENCE", "Power of the brain!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("DTONRAYCII", "DICTIONARY", "A book full of words!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("WUNDTREAER", "UNDERWATER", "A place where fish can live!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("RANORMSTBI", "BRAINSTORM", "An idea that comes suddenly!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("MTHACITAEMS", "MATHEMATICS", "Difficult field of study!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("NLOGTYCHEO", "TECHNOLOGY", "Something that is related to engineering!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("SUTAHLOCO", "HOLOCAUST", "A mass of destruction!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("PRGRMIGNOMA", "PROGRAMMING", "Language used by computers!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("RCSADORSO", "CROSSROAD", "Ways meeting up together!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("LMOGRABHIIN", "LAMBORGHINI", "Fast and expensive car!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("WPOHRSEROE", "HORSEPOWER", "Force that makes a car able to fly!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("PREGOEKAEL", "GOALKEEPER", "Possition on a footbal team!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("LNDSAPCAE", "LANDSCAPE", "Something that's seen in a picture!", (uint)Game.Count + 1));
+            Game.Add(new Anagrams("AMIONHCIPSPI", "CHAMPIONSHIP", "A big competition!", (uint)Game.Count + 1));
+            textBox5.Text = Game[(int)level-1].GetChances().ToString();
+            textBox8.Text = (level-1).ToString();
+            textBox6.Text = Game[(int)level-1].GetInfoClue();
+            textBox4.Text = Game[(int)level-1].GetAnagram();
+            button3.Click += new EventHandler(this.button3_Click); // using this to call the function button3 when it is clicked.
+            //textBox7.KeyDown += new KeyEventHandler(Send);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox7.Text != "")
+            {
+                user_input = textBox7.Text.ToUpper();
+                textBox7.Text = "";
+                if(Game[(int)level-1].CheckAnswear(user_input, ref level))
+                {
+                    textBox9.Text = "Good answear!";
+                    textBox7.Enabled = false;
+                    button3.Enabled = false;
+                    button4.Enabled = true;
+                    if(level==21)
+                    {
+                        textBox9.Text = "Congratulations!";
+                        button4.Text = "End Program";
+                        gameover = true;
+                    }
+                    button4.Click += new EventHandler(this.button4_Click);
+                }
+                else
+                {
+                    textBox9.Text = "Bad answear!";
+                    textBox5.Text = Game[(int)level - 1].GetChances().ToString();
+                    if(Game[(int)level-1].GetChances()<=0)
+                        {
+                            textBox9.Text = "GameOver!";
+                            button4.Enabled = true;
+                            button4.Text = "Quit";
+                            gameover = true;
+                            button4.Click += new EventHandler(this.button4_Click);
+                        }
+                }
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!gameover)
+            {
+                textBox5.Text = Game[(int)level - 1].GetChances().ToString();
+                textBox8.Text = (level - 1).ToString();
+                textBox6.Text = Game[(int)level - 1].GetInfoClue();
+                textBox4.Text = Game[(int)level - 1].GetAnagram();
+                button3.Click += new EventHandler(this.button3_Click);
+                button4.Enabled = false;
+                textBox9.Text = "";
+                textBox7.Enabled = true;
+                button3.Enabled = true;
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+        private void Send(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                user_input = textBox7.Text;
+                //textBox7.Text = "";
+            }
+        }
+    }
+    public class Anagrams
+    {
+        string anagram;
+        uint level;
+        string solution;
+        string info;
+        uint chances;
+        public Anagrams(string Anagram_Word, string Solution_Word, string Information, uint Level_Value)
+        {
+            anagram = Anagram_Word;
+            level = Level_Value;
+            solution = Solution_Word;
+            info = Information;
+            chances = 10;
+        }
+        public bool CheckAnswear(string UserAnswear, ref uint level)
+        {
+            if (UserAnswear.ToUpper() == this.solution)
+            {
+                level++;
+                return true;
+            }
+            else
+            {
+                this.chances--;
+                return false;
+            }
+        }
+        public uint GetLevel()
+		{
+            return this.level ;
+		}
+        public uint GetChances()
+		{
+            return this.chances ;
+		}
+        public string GetInfoClue()
+		{
+            return this.info;
+		}
+        public string GetAnagram()
+        {
+            return this.anagram;
+        }
+    }
+}
